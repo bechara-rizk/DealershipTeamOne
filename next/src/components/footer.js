063c89eb0d9563e9623d0a07270d4fd1106c8de8
AyaAlHaj17 <asa100@mail.aub.edu>
2023-03-12 17:45:09 +0200

    added footer

---
+++ next/src/components/footer.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      
+      <footer>
+       
+      <div class="footer-content">
+        <h3>About Us</h3>
+        <p>Drive your dreams to reality with our unbeatable selection of top-quality cars and exceptional customer service, making your car shopping experience truly unforgettable</p>
+        <div>
+          <h2>Contact Us:</h2>
+        </div>
+        <div class="contact-info">
+        
+          <div>Phone: <a href="tel:+96100000000">+961 00 000 000</a></div>
+          <div>Email: <a href="mailto:info@dealership.com">info@dealership.com</a></div>
+        </div>
+      
+        <ul class="socials">
+          <li><a href=" "><i class= "fa fa-facebook"></i></a></li>
+          <li><a href=" "><i class= "fa fa-twitter"></i></a></li>
+          <li><a href=" "><i class= "fa fa-google-plus"></i></a></li>
+          <li><a href=" "><i class= "fa fa-youtube"></i></a></li>
+          <li><a href=" "><i class= "fa fa-linkedin-square"></i></a></li>
+        </ul>
+        
+        <div class="footer-bottom">
+          <p>copyright &copy; 2023</p>
+        </div>
+      </div>  
+     
+          
+      </footer>
+    </div>
+  );
+}
+
+export default App;
