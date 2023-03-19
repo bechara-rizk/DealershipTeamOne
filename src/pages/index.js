d0b2ad385c0e564faf692f16a2e49e57c31d7be5
TiaHajj <tka15@aub.edu.lb>
2023-03-19 16:16:54 +0200

    landing page+ navbar + styleAll

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import styles from '@/styles/Home.module.css'
 import SignUp from '@/screens/products'
 import About from '@/screens/about'
 import Footer from '@/components/footer'
+import Navbar from '@/components/Navbar'
 
 export default function Home() {
   return (
-    <Footer></Footer>
+    <Navbar></Navbar>
+
   )
 }
