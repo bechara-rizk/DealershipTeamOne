60112dbc1610f12713c29a6aa6c3958a124047d5
TiaHajj <tka15@aub.edu.lb>
2023-03-26 23:39:13 +0300

    navbar styling(TRD)+home screenstyling inprogress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React from "react";
 
+
 const Navbar = () => {
 return (
-	<div className='navbar'>
+	/*<div className='navbar'>
     <a href="/home/homescreen">Home</a>
     <a href="/home/products">Listings</a>
     <a href="/home/about">About Us</a>
     <a href="/home/contactUs">Contact Us</a>
-  </div>
+  </div> */
+  <ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
 );
 };
 
