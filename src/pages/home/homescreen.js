dfbf25ec1681c6c81feb539eac03f59895f19b62
TiaHajj <tka15@aub.edu.lb>
2023-03-29 09:49:07 +0300

    changed image ( minor change)

--- src/pages/home/homescreen.js
+++ src/pages/home/homescreen.js
@@ -1,6 +1,6 @@
 import Navbar from '@/components/Navbar'
 import Image from 'next/image';
-import car from '../../images/pic.jpg'
+import car from '../../images/pic.jpeg'
 import Link from 'next/link';
 
 export default function Home() {
