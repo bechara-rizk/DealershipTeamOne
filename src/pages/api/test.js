b7f948b40541b74f6fb831634a3eb9b0bb25a39b
TiaHajj <tka15@aub.edu.lb>
2023-03-31 09:42:27 +0300

    random

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -3,7 +3,7 @@ import clientPromise from "../../../mongodb";
 export default async function handler(req, res) {
 	const client = await clientPromise;
 	const collection = client.db("Dealership").collection("listings");
-  const documents = await collection.find({}).toArray();
+  	const documents = await collection.find({}).toArray();
 
 	res.status(200).send(documents);
 }
