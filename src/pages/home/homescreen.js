1fc7e06ba0b527d640475678bea509c3586e3780
Bechara Rizk <becharaerizk@yahoo.com>
2023-04-05 21:14:11 +0300

    fixed formatting

--- src/pages/home/homescreen.js
+++ src/pages/home/homescreen.js
@@ -9,17 +9,13 @@ export default function Home() {
       <div className='authButtons'>
         <span className='authElement'>LogIn</span>
       </div>
-
       <Navbar/>
       <h1 className='homeTitle'>A Luxury Car Voyage</h1>
         <div className='discoverHomeButton'>
-          <span className='discoverHomeButton'>Discover <i class="arrow right"></i></span>
-          
+          <span className='discoverHomeButton'>Discover <i className="arrow right"></i></span>
         </div>
       <Footer/>
     </div>
-
-    
     </>
   )
 }
