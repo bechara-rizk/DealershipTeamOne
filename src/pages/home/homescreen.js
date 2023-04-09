289016eb31bacd3bf9e279075e943f1038bddedc
Bechara Rizk <becharaerizk@yahoo.com>
2023-04-09 16:46:25 +0300

    fixed button hover color change

--- src/pages/home/homescreen.js
+++ src/pages/home/homescreen.js
@@ -24,7 +24,7 @@ export default function Home() {
       <div className='homescreenCentered'>
         <h1 className='homeTitle'>A Luxury Car Voyage</h1>
         <div className='discoverHomeButton'>
-          <span className='discoverHomeButton'>Discover <i className="arrow right"></i></span>
+          <span className=''>Discover <i className="arrow right"></i></span>
         </div>
       </div>
     </div>
