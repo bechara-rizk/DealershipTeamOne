363b788407f0acf637b0703198fe0325e463e6e3
TiaHajj <tka15@aub.edu.lb>
2023-05-07 16:42:59 +0300

    Recaptchs keys+installation + filter (incomp)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,18 @@ export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
+const form = () =>{
+
+  return(
+      <form>
+          <label htmlFor="name">Name</label>
+              <input type="text" id="name" className="input"/>
+          <button>Submit</button>
+      </form>
+  )
+}
+
+export default form 
 /*
 const App = () => {
   const handleClick = () => {
