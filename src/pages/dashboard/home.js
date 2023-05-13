b73e24e38b8d17a8231edc6c698853fa4dad7b81
Layan Yamani <layanyamani@gmail.com>
2023-05-13 22:09:38 +0300

    dash in home

--- src/pages/dashboard/home.js
+++ src/pages/dashboard/home.js
@@ -16,8 +16,9 @@ export default function AdminHome() {
     <div className="dashboardScreen">
       <div className="backgroundImage">
       <div className="overlay">
-        <Sidebar />
+
         <DashboardNavbar />
+        <Sidebar />
         </div>
         <img src="/images/road2.jpg" alt="Image Description" style={{'width':'100%', 'marginLeft':100}}/>
       </div>
