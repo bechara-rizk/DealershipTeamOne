0a004bef771d9ab11450c3c2f2a998670fde3afb
TiaHajj <tka15@aub.edu.lb>
2023-05-14 12:26:48 +0300

    reCaptcha login done

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -4,11 +4,13 @@ import Navbar from '@/components/Navbar';
 import { auth } from '../../../firebase';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from 'next/router';
+import ReCAPTCHA from "react-google-recaptcha";
 
 export const Login = (props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
+  const onChange = (value) => {};
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -53,7 +55,10 @@ export const Login = (props) => {
                 id="password"
                 name="password"
               />
-              
+              <ReCAPTCHA
+                sitekey="6LfkQdclAAAAANwmYDcPupslwLU3cvuadhc8oC9x"
+                onChange={onChange}
+              />,
             <button className="Submitbutton" type="submit">Login</button>
             <button className="togglebutton" onClick={() => toggleForm()}>Don't have an account? Sign up here.</button>
             
