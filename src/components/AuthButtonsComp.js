eaac09dc0eea322df37e3867a996ab5297c306c4
AyaAlHaj17 <asa100@mail.aub.edu>
2023-05-15 14:38:21 +0300

    Integrating userprofile/edit profile to homescreen

--- src/components/AuthButtonsComp.js
+++ src/components/AuthButtonsComp.js
@@ -1,48 +1,60 @@
 import { auth } from '../../firebase';
 import { useAuthState } from "react-firebase-hooks/auth";
-
+import UserProfile from './UserProfile';
 
 const AuthButtons = () => {
   const [user, loading] = useAuthState(auth);
+
   if (loading) {
-    // console.log('loading')
-    return(
-      <>
+    return (
       <div className='authButtons'>
-      <img src='/images/logo.jpg' alt='logo' className='homepageLogo'/>
-      <div className="dropdown" style={{'z-index':'-99'}}>
-        <button className="dropbtn">Login <i className="arrowLogin down"></i>
-        <i className="fa fa-caret-down"></i>
-        </button>
-        <div className="dropdown-content">
-          <a href="/auth/Login">Login</a>
-          <a href="/auth/Register">Sign Up</a>
+        <img src='/images/logo.jpg' alt='logo' className='homepageLogo'/>
+        <div className="authContainer">
+          <div className="dropdown" style={{'z-index':'-99'}}>
+            <button className="dropbtn">Login <i className="arrowLogin down"></i>
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content">
+              <a href="/auth/Login">Login</a>
+              <a href="/auth/Register">Sign Up</a>
+            </div>
           </div>
         </div>
       </div>
-      </>
     );
   }
-  return(
-      <>
-      <div className='authButtons'>
+
+  return (
+    <div className='authButtons'>
       <img src='/images/logo.jpg' alt='logo' className='homepageLogo'/>
-      <div className="dropdown">
-        <button className="dropbtn">{!user ? "Login" : "Sign Out"} <i className="arrowLogin down"></i>
-        <i className="fa fa-caret-down"></i>
-        </button>
-        <div className="dropdown-content">
-          { !user ? <>
-          <a href="/auth/Login">Login</a>
-          <a href="/auth/Register">Sign Up</a></>:
-          <>
-                {user.uid === "4rplVi6gQfW4oZSvnXGf1D4z05x2" ? <a href="/dashboard/home">Admin</a>:null}
-          <a href="" onClick={() => auth.signOut()}>Sign out</a>
-              </>}
+      <div className="authContainer">
+        <div className="dropdown">
+          <button className="dropbtn">
+            {!user ? "Login" : "Sign Out"} <i className="arrowLogin down"></i>
+            <i className="fa fa-caret-down"></i>
+          </button>
+          <div className="dropdown-content">
+            {!user ? (
+              <>
+                <a href="/auth/Login">Login</a>
+                <a href="/auth/Register">Sign Up</a>
+              </>
+            ) : (
+              <>
+                {user.uid === "4rplVi6gQfW4oZSvnXGf1D4z05x2" ? <a href="/dashboard/home">Admin</a> : null}
+                <a href="" onClick={() => auth.signOut()}>Sign out</a>
+              </>
+            )}
           </div>
         </div>
+        {user && (
+          <div className="profile">
+            <UserProfile />
+          </div>
+        )}
       </div>
-      </>
-    );
-  };
+    </div>
+  );
+};
+
 export default AuthButtons;
