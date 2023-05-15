c74177cab2fbe8a68545f79658f6f86df86c9efd
DESKTOP-LCD5856\97450 <ike06@mail.aub.edu>
2023-05-15 15:47:14 +0300

    integrated compareCars with database

--- src/pages/home/compareCar.js
+++ src/pages/home/compareCar.js
@@ -1,20 +1,73 @@
 import { Button } from "antd";
 import React from "react";
-import { useState } from 'react';
-import cars from './CarDetails';
+import { useState, useEffect } from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { firestore, storage } from '../../../firebase';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { ref, listAll, getDownloadURL, list } from "firebase/storage";
+
+const fetchCollectionData = async () => {
+  let db = firestore
+  let colRef = collection(db, "listings")
+  let q = query(colRef, where("sold", "==", false))
+  const querySnapshot = await getDocs(q);
+  const documents = querySnapshot.docs.map((doc) => doc.data());
+  return documents;
+};
+
+const fetchStorageData = async (files) => {
+  const listRef = ref(storage, 'images/');
+  let itemReferences = []
+  await listAll(listRef)
+    .then((res) => {
+      res.items.forEach((itemRef) => {
+        itemReferences.push(itemRef)
+      });
+    })
+
+  let images = {}
+  for (let itemRef of itemReferences) {
+    let url = await getDownloadURL(itemRef)
+    images[itemRef.name.slice(0, -4)] = url
+  }
+  return images;
+};
 
 export default function CompareCar() {
 
     const [selectedCar1, setSelectedCar1] = useState('');
     const [selectedCar2, setSelectedCar2] = useState('');
-    const car1 = selectedCar1 && cars.find(car => car.make === selectedCar1.split(' ')[0] && car.model.split(' ')[0] === selectedCar1.split(' ')[1]);
+    const [loading, setLoading] = useState(true)
+    const [listings, setListings] = useState([])
+    const [images, setImages] = useState([])
+
+    const car1 = selectedCar1 && listings.find(car => car.make === selectedCar1.split(' ')[0] && car.model.split(' ')[0] === selectedCar1.split(' ')[1]);
     
-    const car2 = selectedCar2 && cars.find(car => car.make === selectedCar2.split(' ')[0] && car.model.split(' ')[0] === selectedCar2.split(' ')[1]);
+    const car2 = selectedCar2 && listings.find(car => car.make === selectedCar2.split(' ')[0] && car.model.split(' ')[0] === selectedCar2.split(' ')[1]);
     const handleSubmit = (e) => {}
 
     const compare = (e) => {}
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const collectionData = await fetchCollectionData();
+      const storageData = await fetchStorageData(collectionData);
+
+      setListings(collectionData);
+      setImages(storageData);
+      setLoading(false);
+ 
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  console.log(listings)
+  console.log(images)
   return (
     <>
     <div className='authButtons'>
@@ -41,15 +94,15 @@ export default function CompareCar() {
       
       
       <div className="child">
-        <img  src={selectedCar1 && car1 ? car1.picture :  '/images/blackcar.png'}  alt="car image" style={{width: '450px', height: 'auto' }}/>
+        <img  src={selectedCar1 && car1 ? images[car1.VIN] :  '/images/blackcar.png'}  alt="car image" style={{width: '450px', height: 'auto' }}/>
         <form onSubmit={handleSubmit}>
          <h3 className="cars">First Car</h3>
         <label htmlFor="car-Test">Car:</label>
         <select style={{color: "black"}} id="car" value={selectedCar1} onChange={(e) => setSelectedCar1(e.target.value)}>
           <option style={{color: "gray"}} value="Select-A-TestCar">-- Select a car --</option>
-          <option value="Audi A4">Audi A4</option>
-          <option value="BMW 4 series">BMW 4 series</option>
-          <option value="Mercedes-Benz C Class">Mercedes-Benz C-Class</option>
+                {listings.map((listing, index) => (
+                  <option key={index} value={listing.make + " " + listing.model}>{listing.make + " " + listing.model}</option>
+                ))}
           
       
         </select>
@@ -87,15 +140,15 @@ export default function CompareCar() {
 
 
       <div className="child">
-        <img src={selectedCar2 && car2  ? car2.picture : '/images/redcar.jpg'}   alt="car image" style={{width: '450px', height: 'auto' }}/>
+        <img src={selectedCar2 && car2 ? images[car2.VIN]: '/images/redcar.jpg'}   alt="car image" style={{width: '450px', height: 'auto' }}/>
         <form onSubmit={handleSubmit}>
         <h3 className="cars">Second Car</h3>
         <label htmlFor="car-Test">Car:</label>
         <select style={{color: "black"}} id="car" value={selectedCar2} onChange={(e) => setSelectedCar2(e.target.value)}>
           <option  style={{color: "gray"}} value="Select-A-TestCar">-- Select a car --</option>
-          <option  value="Audi A4">Audi A4</option>
-          <option  value="BMW 4 series">BMW 4 series</option>
-          <option  value="Mercedes-Benz C Class">Mercedes-Benz C-Class</option>
+                {listings.map((listing, index) => (
+                  <option key={index} value={listing.make + " " + listing.model}>{listing.make + " " + listing.model}</option>
+                ))}
           
       
         </select>
